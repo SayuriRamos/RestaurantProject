d71dcaf04472efb6e5df24500a7ffbca7900e9ea
Sayuri Ramos Higuera <sayuri@advancio.com>
2023-03-10 11:34:59 -0800

    Finish code and add comments

--- MenuItems/Item.cs
+++ MenuItems/Item.cs
@@ -9,7 +9,7 @@ namespace RestaurantProject.items
     internal class Item
     {
         public string Name { get; set; }
-        public decimal Price { get; set; }
+        public int Price { get; set; }
         public int Time { get; set; }
 
     }
--- Program.cs
+++ Program.cs
@@ -8,37 +8,48 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 
+/* 
+ * This file is simulating a japanese restaurant, where the customer can order from 3 different types of items. 
+ * The customer can view, add, and delete items from cart. 
+ * Checkout is also implemented, with the simulation of "delivery" in a txt file with details of the order (the path may not work for you, please review it beforehand (line 541)).
+ * 
+ * Sayuri Ramos Higuera
+ */
+
 namespace curso_de_net_core
 {
 
     class Program
     {
-        // Define a delegate type that takes a string parameter and returns void
+        // Create a Delegate for main Menu of the restaurant
         public delegate void MenuDelegate(int menuItem);
 
-
+        //create dictionary for the main menu
         public Dictionary<int, MenuDelegate> menu = new Dictionary<int, MenuDelegate>();
 
-
+        //arrays that save the items that are offered
         private static Food[] foodArray = new Food[5];
         private static Drink[] drinkArray = new Drink[3];
         private static Dessert[] dessertArray = new Dessert[3];
 
-        private Object[] itemArray;
-
+        //this is the car list where the products added are saved
         private List<Item> cart = new List<Item>();
         
-
+        
         public static void Main(string[] args)
         {
-            Program creat = new Program();
+            //Fill the arrays with food
             CreateItems();
 
+            
+            Program creat = new Program();
             creat.Create();
            
         }
 
-
+        /// <summary>
+        /// Create items like food, drinks, and desserts
+        /// </summary>
         public static void CreateItems()
         {
             //Add Food
@@ -77,7 +88,7 @@ namespace curso_de_net_core
                 IsSpicy = false
             };
 
-
+            //Add food to array
             foodArray[0] = (onigiri);
             foodArray[1] = (ramen);
             foodArray[2] = (soba);
@@ -110,10 +121,12 @@ namespace curso_de_net_core
 
             };
 
+            //add drinks to array
             drinkArray[0] = (calpis);
             drinkArray[1] = (greenTea);
             drinkArray[2] = (shochu);
 
+            //Add desserts
             Dessert dorayaki = new Dessert
             {
                 Name = "Dorayaki",
@@ -135,33 +148,54 @@ namespace curso_de_net_core
                 IsFrozen = false
             };
 
+            //Add desserts to array
             dessertArray[0] = (yatsuhashi);
             dessertArray[1] = (yukimiDaifuku);
             dessertArray[2] = (dorayaki);
 
         }
 
+        /// <summary>
+        /// Method to add menu options to dictionary
+        /// </summary>
+        public void Create()
+        {
+
+            menu.Add(1, OrderFood);
+            menu.Add(2, OrderDrink);
+            menu.Add(3, OrderDessert);
+            menu.Add(4, SeeCart);
+            menu.Add(5, Checkout);
+
+            OrderItems(menu);
+
+        }
 
 
-        // Define a method that takes a dictionary of menu items and their corresponding delegate methods
+
+        /// <summary>
+        /// The main menu is displayed and delegates are used to reference the methods for each option
+        /// </summary>
+        /// <param name="menu"></param>
         public void OrderItems(Dictionary<int, MenuDelegate> menu)
         {
             // Ask the user to choose a menu item
-            Console.WriteLine("Welcome to Sayuri´s Resutoran");
+            Console.WriteLine("\n\nWelcome to Sayuri´s Resutoran");
 
             while(true) {
                 
-                Console.WriteLine("Please choose an option:");
+                Console.WriteLine("\nPlease choose an option:");
                 Console.WriteLine("1. Order Food");
                 Console.WriteLine("2. Order Drink");
                 Console.WriteLine("3. Order Dessert");
                 Console.WriteLine("4. See Cart");
                 Console.WriteLine("5. Checkout");
 
-
+                
                 try
                 {
-                    int choice = Convert.ToInt16(Console.ReadLine());
+                    // get input from customer
+                    int choice = Convert.ToInt16(Console.ReadLine()); 
 
                     // Call the delegate method for the chosen menu item
                     if (menu.ContainsKey(choice))
@@ -171,26 +205,32 @@ namespace curso_de_net_core
                     }
                     else
                     {
-                        Console.WriteLine("Invalid choice.");
+                        Console.WriteLine("\nInvalid choice, please choose another option");
                     }
                 }
                 catch(Exception)
                 {
-                    Console.WriteLine("Please write a valid option");
+                    Console.WriteLine("\nPlease write a valid option");
                 }
 
             }
         }
 
+        /// <summary>
+        /// Method that is used if the customer wants to add food to the carts, called from the main menu
+        /// </summary>
+        /// <param name="menuItem"></param>
         public void OrderFood(int menuItem)
         {
+            // A counter is used to 
             var cont = 1;
 
             while (true)
             {
-                Console.WriteLine("-- Food --");
+                Console.WriteLine("\n-- Food --");
                 Console.WriteLine("Choose 1 of the options to add to your cart");
 
+                //Print in console the options of food
                 foreach (Food i in foodArray)
                 {
                     Console.WriteLine("{0} ", cont + ". " + i.Name);
@@ -199,31 +239,37 @@ namespace curso_de_net_core
 
                 try
                 {
+                    //get input froom the customer on what food the wants to add to cart
                     int choice = Convert.ToInt16(Console.ReadLine());
 
+                    //the food is added to cart
                     cart.Add(foodArray[choice - 1]);
 
                     break;
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("Please write a valid option");
+                    Console.WriteLine("\nPlease write a valid option");
+                    cont = 1;
                 }
             }
 
-            
-
-
         }
+
+        /// <summary>
+        /// Method that is used if the customer wants to add drinks to the carts, called from the main menu
+        /// </summary>
+        /// <param name="menuItem"></param>
         public void OrderDrink(int menuItem)
         {
             var cont = 1;
 
             while (true)
             {
-                Console.WriteLine("-- Drink --");
+                Console.WriteLine("\n-- Drink --");
                 Console.WriteLine("Choose 1 of the options to add to your cart");
 
+                //Print in console the options of drinks
                 foreach (Drink i in drinkArray)
                 {
                     Console.WriteLine("{0} ", cont + ". " + i.Name);
@@ -240,20 +286,26 @@ namespace curso_de_net_core
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("Please write a valid option");
+                    Console.WriteLine("\nPlease write a valid option");
+                    cont = 1;
                 }
             }
         }
 
+        /// <summary>
+        /// Method that is used if the customer wants to add desserts to the carts, called from the main menu
+        /// </summary>
+        /// <param name="menuItem"></param>
         public void OrderDessert(int menuItem)
         {
             var cont = 1;
 
             while (true)
             {
-                Console.WriteLine("-- Dessert --");
+                Console.WriteLine("\n-- Dessert --");
                 Console.WriteLine("Choose 1 of the options to add to your cart");
 
+                //Print in console the options of desserts
                 foreach (Dessert i in dessertArray)
                 {
                     Console.WriteLine("{0} ", cont + ". " + i.Name);
@@ -270,45 +322,230 @@ namespace curso_de_net_core
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("Please write a valid option");
+                    Console.WriteLine("\nPlease write a valid option");
+                    cont = 1;
                 }
             }
         }
 
+        /// <summary>
+        /// Method to see cart and be able to delete an item or checkout. 
+        /// </summary>
+        /// <param name="menuItem"></param>
         public void SeeCart(int menuItem)
         {
-            Console.WriteLine("-- Cart --");
-
-            Console.WriteLine("Choose 1 of the following options:");
-            var cont = 1;
-            foreach (Item i in cart)
+            // if the carts is empty
+            if (cart.Count == 0)
             {
-                Console.WriteLine("{0} ", cont + ". " + i.Name);
-                cont++;
+                Console.WriteLine("You have no items in your cart, please add some before checkout :D");
+            }
+            else
+            {
+
+                Console.WriteLine("\n------- Cart -------");
+                var cont = 1;
+                foreach (Item i in cart)
+                {
+                    Console.WriteLine("{0} ", cont + ". " + i.Name + "  $" + i.Price);
+                    cont++;
+                }
+
+
+                while (true)
+                {
+                    Console.WriteLine("\nChoose 1 of the following options:");
+                    Console.WriteLine("1. Delete items from cart" +
+                        "\n2. Checkout");
+
+                    try
+                    {
+                        int choice = Convert.ToInt16(Console.ReadLine());
+
+                        if (choice == 1)
+                        {
+                            
+                            DeleteItem();
+
+                            //print new cart
+                            Console.WriteLine("\n------- NEW Cart -------");
+                            cont = 1;
+
+                            foreach (Item i in cart)
+                            {
+                                Console.WriteLine("{0} ", cont + ". " + i.Name + "  $" + i.Price);
+                                cont++;
+                            }
+
+                            break;
+                        }
+                        else if (choice == 2)
+                        {
+                            Checkout(5);
+                            break;
+                        }
+                        else
+                        {
+                            //trow exception if other numbers are written
+                            throw new Exception("\nThe number is not valid");
+
+                        }
+
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("\nPlease write a valid option");
+                        cont = 1;
+                    }
+                };
             }
 
+
         }
 
+        /// <summary>
+        /// Method to do checkout of the cart
+        /// </summary>
+        /// <param name="menuItem"></param>
         public void Checkout(int menuItem)
         {
-            Console.WriteLine("");
-            Console.WriteLine("Choose 1 of the options to add to your cart");
+            // if the cart is empty, you cannot checkout
+            if (cart.Count == 0)
+            {
+                Console.WriteLine("\nBefore checkout, please add items to the cart");
+            }
+            else {
+                while (true)
+                {
+                    //create a string to save all the text with the items and the total, this is goign to be saved in a txt file
+                    string deliveryText = "Item & Price";
 
+                    var cont = 1;
+                    Console.WriteLine("\n-- Cart --");
+                    Console.WriteLine("Item & Price");
+
+                    
+
+                    foreach (Item i in cart)
+                    {
+                        deliveryText = deliveryText + "\n" + cont + ". " + i.Name + "  $" + i.Price;
+                        Console.WriteLine("{0} ", cont + ". " + i.Name + "  $" + i.Price);
+
+                        cont++;
+                    }
+
+                    // string that saves the total price
+                    string total = totalCart();
+                    Console.WriteLine(total);
+
+                    deliveryText = deliveryText + "\n" + total;
+
+                    //Gives the option to customer to cancel or confirm checkout
+                    Console.WriteLine("\n\nPlease choose and option: " +
+                        "\n1. Confirm Checkout" +
+                        "\n2. Cancel");
+
+                    try
+                    {
+                        int choice = Convert.ToInt16(Console.ReadLine());
+
+                        if (choice == 1)
+                        {
+                            delivery(deliveryText);
+                            Environment.Exit(0);
+                        }
+                        else if (choice == 2)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            //trow exception if other numbers are written
+                            throw new Exception("\nThe number is not valid");
+
+                        }
+
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("\nPlease write a valid option");
+                        cont = 1;
+                    }
+                }
+            }
         }
 
-      
 
-
-        public void Create ()
+        /// <summary>
+        /// Method that deleted an item from cart
+        /// </summary>
+        public void DeleteItem()
         {
+            while (true)
+            {
+                var cont = 1;
+                Console.WriteLine("\n-- Cart --");
+                Console.WriteLine("Select the item to delete");
 
-            menu.Add(1, OrderFood);
-            menu.Add(2, OrderDrink);
-            menu.Add(3, OrderDessert);
-            menu.Add(4, SeeCart);
-            menu.Add(5, Checkout);
-            
-            OrderItems(menu);
+                //print items in cart
+                foreach (Item i in cart)
+                {
+                    Console.WriteLine("{0} ", cont + ". " + i.Name + "  $" + i.Price);
+                    cont++;
+                }
+
+                try
+                {
+                    int choice = Convert.ToInt16(Console.ReadLine());
+
+                    //remove from list
+                    cart.Remove(cart[choice - 1]);
+
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nPlease write a valid option");
+                    cont = 1;
+                }
+                
+            };
+        }
+
+        /// <summary>
+        /// Method to calculate the total price of the order
+        /// </summary>
+        /// <returns></returns>
+        public string totalCart()
+        {
+            int total = 0;
+
+            //iterates through the cart and adds all the prices into one variable 
+            foreach (Item i in cart)
+            {
+                total = total + i.Price;
+            }
+            string totalCart = "\nTotal = " + total.ToString();
+
+            return totalCart;
+        }
+
+        /// <summary>
+        /// Create a txt file to save the order and simulatte delivery
+        /// </summary>
+        /// <param name="totalcart"></param>
+        public void delivery(string totalcart)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            //The txt file is saved on the desktop, please change it if you want it saved in another folder
+            var  path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                              "delivery.txt");
+
+            File.WriteAllText(path, totalcart);
+
+            //final message
+            Console.WriteLine("\n\n\nThe delivery is done!" +
+                "\n thank you for shopping with us\n\n\n");
 
         }
 
